ad732181598c7f3b19b315240b6e6d5c3d16ee9c
yoratoni <yoratoni.dev@gmail.com>
2024-10-14 12:39:44 +0200

    Added app status support

--- types/environment.d.ts
+++ types/environment.d.ts
@@ -11,6 +11,10 @@ declare global {
 
 			// The domain of the application
 			NEXT_PUBLIC_DOMAIN: string
+
+			// Status of the application (allows to enable/disable the application)
+			// Can either be "enabled" | "disabled" | "in-maintenance" | "in-development"
+			NEXT_PUBLIC_APP_STATUS: "enabled" | "disabled" | "in-maintenance" | "in-development"
 		}
 	}
 }
