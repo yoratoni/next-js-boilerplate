9dd00e76f5df2ceee21db4e412f46ea70ad640e5
yoratoni <yoratoni.dev@gmail.com>
2024-10-14 13:28:07 +0200

    Added missing page

---
+++ pages/unavailable/index.tsx
@@ -0,0 +1,8 @@
+export default function Unavailable() {
+	return (
+		<div className="flex flex-col items-center justify-center w-full h-screen bg-black">
+			<h1 className="text-6xl">Unavailable</h1>
+			<p className="text-lg">Sorry, this website is currently unavailable.</p>
+		</div>
+	)
+}
