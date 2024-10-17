742f325387655465455d8ee82ada4a8a256997f2
yoratoni <yoratoni.dev@gmail.com>
2024-10-18 00:36:36 +0200

    Added a bit of overloading on getStatus API CRUD

--- lib/crud/status.ts
+++ lib/crud/status.ts
@@ -3,12 +3,16 @@ import type { RequestResult } from "@/types/requests"
 
 const baseUrl = "/api/status"
 
+// This function is overloaded to allow for two different return types.
+export async function getStatus(markerOnly: true): Promise<RequestResult<string>>
+export async function getStatus(markerOnly: false): Promise<RequestResult<Status>>
+
 /**
  * Get the status of the application.
- * @param markerOnly Whether to only return the marker.
+ * @param markerOnly Whether to only return the marker (optional, defaults to false).
  * @returns The status of the application, or the marker if `markerOnly` is true.
  */
-export async function getStatus(markerOnly: boolean): Promise<RequestResult<Status | string>> {
+export async function getStatus(markerOnly = false): Promise<RequestResult<Status | string>> {
 	const response = await fetch(`${baseUrl}?markerOnly=${markerOnly}`)
 
 	const result = await response.json()
