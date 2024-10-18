04b6ff682741dca1fdf31f8e7ce93f6ddb1d13c5
yoratoni <yoratoni.dev@gmail.com>
2024-10-18 23:01:31 +0200

    Fix

--- lib/crud/cgas.ts
+++ lib/crud/cgas.ts
@@ -10,19 +10,17 @@
 import type { Status } from "@/pages/api/cgas/status"
 import type { RequestResult } from "@/types/requests"
 
-const baseUrl = "/api/status"
-
-// This function is overloaded to allow for two different return types.
-export async function getStatus(markerOnly: true): Promise<RequestResult<string>>
-export async function getStatus(markerOnly?: false): Promise<RequestResult<Status>>
+const baseUrl = "/api/cgas"
 
 /**
  * Get the status of the application.
  * @param markerOnly Whether to only return the marker (optional, defaults to false).
  * @returns The status of the application, or the marker if `markerOnly` is true.
  */
+export async function getStatus(markerOnly: true): Promise<RequestResult<string>>
+export async function getStatus(markerOnly?: false): Promise<RequestResult<Status>>
 export async function getStatus(markerOnly = false): Promise<RequestResult<Status | string>> {
-	const response = await fetch(`${baseUrl}?markerOnly=${markerOnly}`)
+	const response = await fetch(`${baseUrl}/status?markerOnly=${markerOnly}`)
 
 	const result = await response.json()
 	return result
